a5ae1ca2928767d3e02af07aa11fe11972e50ee8
verttb <alyssonoliveira456@gmail.com>
2024-04-28 17:19:07 -0300

    tabela clicavel

---
+++ static/main.js
@@ -0,0 +1,40 @@
+var lista = []
+testeClick();
+
+window.addEventListener('DOMContentLoaded', (event) =>{
+    var tema = localStorage.getItem('theme')
+    if(tema != null){
+      document.body.classList.add(tema)
+    }
+    
+  });
+
+      document.querySelector("#changeTheme").addEventListener("click", () => {
+      document.body.classList.toggle("mudar-tema");
+      tema_atual = document.body.classList.contains('mudar-tema') ? 'mudar-tema' : ''
+      localStorage.setItem('theme', tema_atual);
+});
+
+  function armazenaTexto(){
+      let texto = document.querySelector('input').value
+      localStorage.setItem('texto', texto);
+  }
+
+  window.onload = function(){
+    var texto = localStorage.getItem('texto');
+    document.querySelector('input').setAttribute('value', texto);
+    testeClick();
+  }
+
+function testeClick() {
+    var lista = document.querySelectorAll('tr');
+    lista.forEach(function(elemento) {
+        elemento.addEventListener('click', function() {
+            console.log(this.innerText);
+            filhos = this.getElementsByTagName('td');
+            console.log(filhos[2].innerText);
+        });
+    });
+}
+
+
