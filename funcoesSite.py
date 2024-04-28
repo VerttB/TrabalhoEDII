20e1a56894b67a2853e3ae09d737ef0e742b436c
verttb <alyssonoliveira456@gmail.com>
2024-04-28 12:50:07 -0300

    ajuste na pesquisa

--- funcoesSite.py
+++ funcoesSite.py
@@ -1,10 +1,29 @@
 
+#def get_pagina(dados, inicio, qtd_por_pagina, pagina):
+    # novo_dicionario = {}
+    # inicio+=1 #O indice tem que começar em 1
+    # i = (qtd_por_pagina * pagina) + 1
+    # for item_id, item in dados.items():
+    #     if inicio <= i < (inicio + qtd_por_pagina):
+    #         id = int(item_id)
+    #         id = i + id
+    #         novo_dicionario[str(id)] = item
+    #     i+=1
+    # return novo_dicionario
+
 def get_pagina(dados, inicio, qtd_por_pagina):
     novo_dicionario = {}
-    inicio+=1 #O indice tem que começar em 1
+    tam = 0
+        
     for item_id, item in dados.items():
-        if inicio <= int(item_id) < (inicio + qtd_por_pagina):
+        inicio+=1
+        if inicio < int(item_id) or inicio == int(item_id):
+            while inicio == int(item_id): inicio+=1
             novo_dicionario[item_id] = item
+            tam+=1
+        if inicio > int(item_id): inicio-=1
+        if tam == qtd_por_pagina : break
+    
     return novo_dicionario
 
 def filtrarDicionario(dados,texto=None):
