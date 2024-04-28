ab1c54153e11b5fb93acf49111abaca7488bb2a7
Cainan <cainan.bas@gmail.com>
2024-04-28 16:03:12 -0300

    Adicionar e remover

--- dicionario.py
+++ dicionario.py
@@ -1,22 +1,26 @@
 import json
 
-def adicionarProduto(dict, nome, quantidade, preco, descricao):
-    id = len(dict)
-    dict[id]
+def adicionarProdutoatalogo(dict, nome, quantidade, preco, descricao):
+    id = len(dict)+1
+    nova = dict(nome=nome, quantidade=quantidade, preco=preco, descricao=descricao)
+    dict[id] = nova
     pass
-def removerProduto(id, dict):
+
+def removerProduto(dict, id_delete):
     for id in dict:
-        if(id == dict[id]):
+        if(id_delete == id):
             dict[id].pop()
 
-def atualizarPesquisa():
+def modificarProduto(dict, ):
     pass
-def modificarProduto():
-    pass
+
+
 def atualizarArquivo(dict):
     with open('arquivos/catalogo.json', 'w') as arquivo:
         arquivo.write(json.dumps(dict)) 
     pass
+
+
 def lerArquivo():
     dados = {}
     with open('arquivos/catalogo.json', 'r') as arquivo:
