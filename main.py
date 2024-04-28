9712658ae1073f014ac7a598fc2c7e0151edc393
verttb <alyssonoliveira456@gmail.com>
2024-04-28 13:41:59 -0300

    catalogo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,url_for,render_template,send_file,request
+from flask import Flask,url_for,render_template,send_file,request, session
 from flask_paginate import Pagination, get_page_parameter
 import funcoesSite
 import os
@@ -7,14 +7,25 @@ import dicionario
 # inicializaçaõ
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
+app.secret_key = 'chave'
 
 @app.route('/')
 def principal():
     return render_template('index.html')
 
-@app.route('/catalogo')
-def catalogo(nomeAprocurar=None):
-    catalogo = dicionario.lerArquivo()  # Supondo que lerArquivo() é uma função que retorna o dicionário
+@app.route('/catalogo', methods=['GET', 'POST'])
+def catalogo():
+  
+    catalogo = dicionario.lerArquivo()
+    if(request.method == 'POST'):
+        nomeAprocurar = request.form.get('texto', '')
+    else:
+         nomeAprocurar = request.args.get('texto', '')
+
+
+    session['nome_a_procurar'] = nomeAprocurar
+
+
     if(nomeAprocurar is not None and nomeAprocurar != ''):
         catalogo = funcoesSite.filtrarDicionario(catalogo, nomeAprocurar)
     
@@ -22,8 +33,8 @@ def catalogo(nomeAprocurar=None):
     pagina = request.args.get(get_page_parameter(), type=int, default=1)
     qtd_por_pagina = 15
     pagination_data = funcoesSite.get_pagina(catalogo, (pagina - 1) * qtd_por_pagina, qtd_por_pagina)
-    paginacao = Pagination(page=pagina, total=total, qtd_per_page=qtd_por_pagina, per_page = qtd_por_pagina,search =False, format_number=True, aria_label='e')  
-    return render_template('catalogo.html', paginacao = paginacao, catalogo = pagination_data)
+    paginacao = Pagination(page=pagina, total=total, qtd_per_page=qtd_por_pagina, per_page = qtd_por_pagina,search =False, format_number=True)  
+    return render_template('catalogo.html', paginacao = paginacao, catalogo = pagination_data, nomeAprocurar = nomeAprocurar)
 
 @app.route('/about')
 def about():
@@ -34,24 +45,4 @@ def download():
     path = 'arquivos/catalogo.json'
     return send_file(path, as_attachment=True)
 
-@app.route('/lerTexto', methods=['POST', 'GET'])
-def lerTexto():
-    texto = ''
-    if request.method == 'POST':
-        texto = request.form.get('texto')
-        print(f"Texto {texto}")
-        return catalogo(texto)
-    else:
-        return catalogo(texto)
-   # return render_template('about.html')
-
-# @app.route('/index')
-# def sobre3():
-#     return render_template('index.html')
-
-#exemplo no navegador
-#http://localhost:5000/paginaEDII
-
-
-#execução
 app.run(debug = True)
