9368f7f0c5c3caeb38f5b3cec725fd28f0848c3e
Cainan <137329310+Cainan-bas@users.noreply.github.com>
2024-04-29 23:00:52 -0300

    nem sei

--- dicionario.py
+++ dicionario.py
@@ -7,9 +7,7 @@ def adicionarProdutoatalogo(dict, nome, quantidade, preco, descricao):
     pass
 
 def removerProduto(dict, id_delete):
-    for id in dict:
-        if(id_delete == id):
-            dict[id].pop()
+    dict[id_delete].pop()
     atualizarArquivo(dict)
 
 def modificarProduto(dict, id_alterar, tipo, text):
@@ -31,22 +29,3 @@ def lerArquivo():
     with open('arquivos/catalogo.json', 'r') as arquivo:
         dados = json.load(arquivo)
     return dados
-
-
-    if len(dict) == 0:
-        print("O Dicionario está vazio")
-    else:
-        for chave, valores in sorted(dict.items()):
-            print(chave, ":", valores)
-
-
-    print("""
-          1 - Adicionar ao dicionário
-          2 - Listar Dicionario
-          3 - Tirar do Dicionario
-          4 - Salvar em Arquivo
-          5 - Carregar Arquivo
-          6 - Adicionar Relações
-          7 - Buscar
-          Diga a opção que voce quer.
-          """)
