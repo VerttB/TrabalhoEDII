aaac3a88517052c23584216c2a0120d06a5899e7
verttb <alyssonoliveira456@gmail.com>
2024-05-22 17:12:33 -0300

    trye e except

--- adicionarcampo.py
+++ adicionarcampo.py
@@ -8,19 +8,24 @@ def getLink(nome):
     text = requests.get(f'https://www.google.com/search?q=medicamento:{nome}').text
     html = BeautifulSoup(text, 'html.parser')
     print(nome)
-    htmlTodo = html.find('img').find_next('img')
-    print(htmlTodo)
-    if htmlTodo is not None:
-        return htmlTodo.get('src')
-    else:
+    try:
+        htmlTodo = html.find('img').find_next('img')
+        if htmlTodo is not None:
+            return htmlTodo.get('src')
+        else:
+            return ""
+        
+    except AttributeError:
+        print("Html n encontrado")
         return ""
+    
         
 
 
 dados = dicionario.lerArquivo()
 #try:
 for itens in dados:
-    path = f"static/assets/imagensRemedios/{dados[itens]['nome']}.jpg"
+    path = f"static/assets/imagensRemedios/{dados[itens]['nome']}.png"
     try:
         urllib.request.urlretrieve(getLink(dados[itens]['nome']), path)
     except ValueError:
