18c46fbfae763f5f2987eb28c99dad12148a0c8a
verttb <alyssonoliveira456@gmail.com>
2024-05-22 22:10:45 -0300

    coisas

--- adicionarcampo.py
+++ adicionarcampo.py
@@ -5,12 +5,13 @@ import urllib.request
 
 
 def getLink(nome):
-    text = requests.get(f'https://www.google.com/search?q=medicamento:{nome}').text
+    text = requests.get(f'https://www.google.com/search?q=medicamento:{nome}&tbm=isch').text
     html = BeautifulSoup(text, 'html.parser')
     print(nome)
     try:
         htmlTodo = html.find('img').find_next('img')
         if htmlTodo is not None:
+            print("Retorno feito")
             return htmlTodo.get('src')
         else:
             return ""
@@ -25,11 +26,12 @@ def getLink(nome):
 dados = dicionario.lerArquivo()
 #try:
 for itens in dados:
-    path = f"static/assets/imagensRemedios/{dados[itens]['nome']}.png"
+    path = f"static/assets/imagensRemedios/{dados[itens]['nome']}.jpeg"
     try:
         urllib.request.urlretrieve(getLink(dados[itens]['nome']), path)
     except ValueError:
         print(f"Link do {dados[itens]['nome']} não obitido")
+
     dados[itens]['link'] = path
  
 dicionario.atualizarArquivo(dados)
