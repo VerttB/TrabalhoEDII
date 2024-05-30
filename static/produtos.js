6a2dee742e9b406cf8391e8c53bc217ecabafa07
verttb <alyssonoliveira456@gmail.com>
2024-05-29 22:01:38 -0300

    carrinho de compras, sem design ainda

--- static/produtos.js
+++ static/produtos.js
@@ -1,21 +1,41 @@
 const gridBoxes = document.querySelectorAll('#box');
-listaProdutos = []
+const compras = document.getElementById('compras');
+const qtdCompras = document.getElementById('qtdCompras')
+const listaProdutos = []
 
 const produto = {
-    nome: "nome",
-    preco: "preco",
-    imagemSrc: "path"
-};
+    nome: "",
+    preco: "",
+    imagemSrc: "",
+    quantidade: "",
+}
 
 gridBoxes.forEach( (gridBox) =>{
     gridBox.addEventListener('click', () => {
         nome = gridBox.querySelector('#nome');
         preco = gridBox.querySelector('#preco');
         imagemSrc = gridBox.querySelector('img');
-        produto.nome = nome.textContent
-        produto.preco = preco.textContent
-        produto.imagemSrc = imagemSrc.src
-        listaProdutos.push(produto)
+        adicionaProdutos(nome,preco,imagemSrc);
+        qtdCompras.textContent = Number(qtdCompras.textContent) + 1;
+        
     })
 })
 
+
+function adicionaProdutos(nome,preco,imagemSrc){
+    
+
+if(!listaProdutos.some(p => p['nome'] === nome.textContent)){
+    produto.nome = nome.textContent;
+    produto.preco = preco.textContent;
+    produto.imagemSrc = imagemSrc.src;
+    produto.quantidade = 1;
+    console.log("Entrei aqui");
+    listaProdutos.push(produto);
+
+}
+else{
+  const produtoAchado = listaProdutos.find(p => p.nome === nome.textContent);
+  produtoAchado.quantidade +=1;
+}
+}
