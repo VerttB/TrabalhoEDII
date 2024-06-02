2e8f8ffc486ab06eff6e053f31795652aca501b7
Cainan <cainan.bas@gmail.com>
2024-06-02 14:31:15 -0300

    Agora ta certo

---
+++ BTree.py
@@ -0,0 +1,47 @@
+from BTreeNode import BTreeNode
+
+class BTree:
+
+    def __init__(self, grau, tipo):
+        self.tipo = tipo
+        self.grau = grau
+        self.raiz = BTreeNode(grau, True)
+
+    def inserir(self, valor):
+        raiz = self.raiz
+        if len(raiz.key) == 2 * self.grau - 1:
+            temp = BTreeNode(self.grau, False)
+            self.raiz = temp
+            temp.filho.insert(0, raiz)
+            temp.separaFilhos(0, raiz)
+            temp.inserir_no(valor)
+        else:
+            raiz.inserir_no(valor)
+            
+    def monta_dicio(self, dicio, dicio_atual, dado):
+        for item_id, produto in dicio.items():
+            if produto[self.tipo] == dado:
+                dicio_atual[item_id] = produto
+        return dicio_atual
+        
+    def dicioOrdemCrescente(self, node, dicio, dicio_atual):
+        if node is not None:  
+            for i in range(len(node.key)):
+                if not node.folha:
+                    dicio_atual = self.dicioOrdemCrescente(node.filho[i], dicio, dicio_atual)  
+                dicio_atual = self.monta_dicio(dicio, dicio_atual, node.key[i])  
+            if not node.folha:
+                dicio_atual = self.dicioOrdemCrescente(node.filho[len(node.key)], dicio, dicio_atual)  
+        return dicio_atual
+    
+    def dicioOrdemDecrescente(self, node, dicio, dicio_atual):
+        if node is not None:
+            if not node.folha:
+                dicio_atual = self.dicioOrdemDecrescente(node.filho[len(node.key)], dicio, dicio_atual)
+            for i in range(len(node.key) - 1, -1, -1):
+                dicio_atual = self.monta_dicio(dicio, dicio_atual, node.key[i])
+                if not node.folha:
+                    dicio_atual = self.dicioOrdemDecrescente(node.filho[i], dicio, dicio_atual)
+        return dicio_atual
+    
+    # def dicio_In_Range():
---
+++ BTreeNode.py
@@ -0,0 +1,39 @@
+class BTreeNode:
+    # key são os nossos valores da arvore
+
+    # Define os atributos padrão do node da arvoreB
+    def __init__(self, ordem, folha = False):
+          self.ordem = ordem
+          self.folha = folha
+          self.index = 0
+          self.filho = []
+          self.key = []
+
+    def inserir_no(self, key):
+        i = len(self.key)-1 # i recebe a quantidade de valores que tem no nó
+        if self.folha:
+            self.key.append(0)
+            while i >= 0 and self.key[i] > key:
+                self.key[i+1] = self.key[i]
+                i -= 1
+            self.key[i+1] = key
+
+        else:
+            while i >= 0 and self.key[i] > key:
+                i -= 1
+            if len(self.filho[i+1].key) == 2 * self.ordem - 1:
+                self.separaFilhos(i + 1, self.filho[i + 1])
+                if self.key[i + 1] < key:
+                    i += 1
+            self.filho[i + 1].inserir_no(key)
+                  
+
+    def separaFilhos(self, i, noFilho):
+        noNovo = BTreeNode(noFilho.ordem, noFilho.folha) # Cria um novo nó
+        self.filho.insert(i+1, noNovo) # Nó pai aponta para o novo nó
+        self.key.insert(i, noFilho.key[self.ordem - 1]) # Move a chave mediana para o nó pai
+        noNovo.key = noFilho.key[self.ordem:(2 * noFilho.ordem - 1)] # Move as chaves depois da mediana do filho para o novo no
+        noFilho.key = noFilho.key[0:(self.ordem - 1)] # O noFilho mantem as chaves até ordem -1
+        if not noFilho.folha:
+             noNovo.filho = noFilho.filho[self.ordem:(2 * noFilho.ordem)]
+             noFilho.filho = noFilho.filho[0:self.ordem]
