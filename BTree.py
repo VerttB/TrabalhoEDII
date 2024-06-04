238b9ae5a085224315e27067bec3874f3a1b8af3
Cainan <cainan.bas@gmail.com>
2024-06-03 23:11:45 -0300

    update BTree

--- BTree.py
+++ BTree.py
@@ -18,11 +18,22 @@ class BTree:
         else:
             raiz.inserir_no(valor)
             
+    #--------------------------------------------------------
+    
     def monta_dicio(self, dicio, dicio_atual, dado):
         for item_id, produto in dicio.items():
-            if produto[self.tipo] == dado:
+            if str(produto[self.tipo]) == dado:
                 dicio_atual[item_id] = produto
         return dicio_atual
+    
+    def monta_dicio_range(self, dicio, dicio_atual, dado):
+        for item_id, produto in dicio.items():
+            if str(produto[self.tipo]) == dado:
+                dicio_atual[item_id] = produto
+                return dicio_atual
+        return dicio_atual
+    
+    #--------------------------------------------------------
         
     def dicioOrdemCrescente(self, node, dicio, dicio_atual):
         if node is not None:  
@@ -44,7 +55,32 @@ class BTree:
                     dicio_atual = self.dicioOrdemDecrescente(node.filho[i], dicio, dicio_atual)
         return dicio_atual
     
-    # def dicio_In_Range():
+    def pesquisaArvore(self, node, dicio, dicio_atual, texto):
+        if node is not None:
+            if not node.folha:
+                dicio_atual = self.pesquisaArvore(node.filho[len(node.key)], dicio, dicio_atual, texto)
+            for i in range(len(node.key)):
+                if node.key[i] == texto:
+                    dicio_atual = self.monta_dicio(dicio, dicio_atual, node.key[i])
+                    return dicio_atual
+                if not node.folha:
+                    dicio_atual = self.pesquisaArvore(node.filho[i], dicio, dicio_atual, texto)
+        return dicio_atual
+    
+    def dicio_In_Range(self, node, dicio, dicio_atual, cont, min, max):
+        if node is not None:
+            for i in range(len(node.key)):
+                if not node.folha:
+                    dicio_atual, cont = self.dicio_In_Range(node.filho[i], dicio, dicio_atual, cont, min, max)
+                if min <= cont <= max:
+                    dicio_atual = self.monta_dicio_range(dicio, dicio_atual, node.key[i])
+                cont += 1
+            if not node.folha:
+                dicio_atual, cont = self.dicio_In_Range(node.filho[len(node.key)], dicio, dicio_atual, cont, min, max)
+        return dicio_atual, cont
+
+
+
 
 
 arvoreNome = BTree(grau=5, tipo='nome')
